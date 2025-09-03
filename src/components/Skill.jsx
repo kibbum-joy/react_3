30cfb176b6ec776756f823a811d30dd63525e1ee
kibbum-joy <qlzl874@daum.net>
2025-09-03 13:10:37 +0900

    Add files via upload

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 
+const skillText = [
+  {
+    titie: "Lorem ipsum dolor sit amet.",
+    desc: "--Lorem ipsum, dolor sit amet consectetur adipisicing elit. At velit fugiat ipsam, provident ut praesentium neque consectetur a corporis. Molestiae sint officia ut aliquam debitis consequatur autem? Magni, reiciendis cum!",
+  },
+  {
+    titie: "-Lorem ipsum dolor sit amet.",
+    desc: "Lorem --ipsum, dolor sit amet consectetur adipisicing elit. At velit fugiat ipsam, provident ut praesentium neque consectetur a corporis. Molestiae sint officia ut aliquam debitis consequatur autem? Magni, reiciendis cum!",
+  },
+  {
+    titie: "Lorem -ipsum dolor sit amet.",
+    desc: "Lorem ipsum, --dolor sit amet consectetur adipisicing elit. At velit fugiat ipsam, provident ut praesentium neque consectetur a corporis. Molestiae sint officia ut aliquam debitis consequatur autem? Magni, reiciendis cum!",
+  },
+  {
+    titie: "Lorem ipsum -dolor sit amet.",
+    desc: "Lorem ipsum, dolor --sit amet consectetur adipisicing elit. At velit fugiat ipsam, provident ut praesentium neque consectetur a corporis. Molestiae sint officia ut aliquam debitis consequatur autem? Magni, reiciendis cum!",
+  },
+]
+
 const Skill = () => {
   return (
-    <div>Skill</div>
+    <section id='skill'>
+      <div className="skill_inner">
+        <h2 className='skill_title'>
+          Challenge <em>나의 도전</em>
+        </h2>
+        <div className="skill_desc">
+
+          {
+            skillText.map((skill, key) => (
+              <div key={key}>
+                <span>{key + 1}.</span>
+                <h3>{skill.titie}</h3>
+                <p>{skill.desc}</p>
+              </div>
+            ))
+          }
+
+        </div>
+      </div>
+    </section>
   )
 }
 
